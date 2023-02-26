d710dd3644e4ab680bf0ba544ff6e36cb73cfb5b
arikrayi <arikrayi@gmail.com>
2023-02-26 20:48:26 +0700

    style: css variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import "@fontsource/open-sans/variable-full.css"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h1>Title</h1>
+      <h2>Subtitle</h2>
+      <p>Body</p>
+      <button>Primary</button>
+      <button className="secondary">Secondary</button>
     </div>
   )
 }
