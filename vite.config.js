155e5d90f23cfb86e23460583f9b779b549efa32
Adiyansa Prasetya Wicaksana <adiyansawicaksana@gmail.com>
2023-06-22 11:34:25 +0700

    config: custom accessing material

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, searchForWorkspaceRoot } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
@@ -6,6 +6,9 @@ export default defineConfig({
     server: {
         host: '0.0.0.0',
         port: 3000,
+        fs: {
+            allow: [searchForWorkspaceRoot(process.cwd()), '..'],
+        },
     },
     plugins: [react()],
 });
